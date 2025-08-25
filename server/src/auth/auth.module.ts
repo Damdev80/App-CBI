26bab21d917e0dd8c1b8b0cee15636c0cca67536
Damdev80 <danielbuelvas9624@gmail.com>
2025-08-24 22:32:48 -0500

    Refactor AuthModule and AuthService to integrate JWT authentication, improve error handling, and enhance user login logic

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
-
+import { UsersModule } from 'src/users/users.module';
+import { JwtModule } from '@nestjs/jwt';
 @Module({
+  imports: [
+    UsersModule,
+    JwtModule.register({
+      global: true,
+      secret: process.env.TOKEN_SECRET,
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
   controllers: [AuthController],
 })
 export class AuthModule {}
--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,23 +1,42 @@
-import { Injectable } from '@nestjs/common';
-import { PrismaService } from 'src/prisma/prisma.service';
+import {
+  BadRequestException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { UsersService } from 'src/users/users.service';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class AuthService {
   constructor(
-    private readonly PrismaService: PrismaService,
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    userService: UsersService,
+    private userService: UsersService,
+    private jwtService: JwtService,
+    private prismaService: PrismaService,
   ) {}
 
-  async login(email: string, password: string) {
-    const user = await this.PrismaService.user.findUnique({
-      where: { email },
-    });
+  async logIn(email: string, password: string) {
+    try {
+      const user = await this.prismaService.user.findUnique({
+        where: {
+          email,
+        },
+      });
+      if (!user) {
+        throw new BadRequestException('Email o contraseña invalidos');
+      }
+    } catch (error) {}
   }
 
-  async register() {
-    //Logica de validacion
-    return this.PrismaService.user.create({});
+  async signIn(name: string, pass: string): Promise<{ access_token: string }> {
+    const user = await this.userService.findById(name);
+    if (user?.password !== pass) {
+      throw new UnauthorizedException();
+    }
+
+    const payload = { sub: user.id, username: user.name };
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+    };
   }
 }
