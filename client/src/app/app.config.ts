54321268bab171aa1346f4c82085bfcdf40ea8c1
Damdev80 <danielbuelvas9624@gmail.com>
2025-09-05 23:04:03 -0500

    feat: update Angular configuration to include component schematics and enhance app routing

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,13 +1,17 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes, withComponentInputBinding()),
+    provideHttpClient(),
+    provideClientHydration(withEventReplay())
+    
   ]
 };
