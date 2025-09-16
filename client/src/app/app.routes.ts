94c8d0e4781024a721519fc791649a8f3942b6ab
Damdev80 <danielbuelvas9624@gmail.com>
2025-09-16 18:58:04 -0500

    fix: update routing to use direct component reference for NotFoundComponent and improve formatting

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -3,19 +3,30 @@ import { PublicLayoutComponent } from './layout/public-layout/public-layout.comp
 import { WelcomeComponent } from './pages/welcome/welcome.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { NotFoundComponent } from './pages/not-found/404.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: PublicLayoutComponent,
         children: [
-            {path: '', component: WelcomeComponent, pathMatch: 'full'},
-            {path: 'login', component:LoginComponent},
-            {path: 'register', component: RegisterComponent}
+            {
+                path: '',
+                component: WelcomeComponent, 
+                pathMatch: 'full'
+            },
+            {
+                path: 'login', 
+                component:LoginComponent
+            },
+            {
+                path: 'register', 
+                component: RegisterComponent
+            }
         ]
     },
     {
         path:'**',
-        loadComponent: () => import('./pages/not-found/404.component').then(c => c.NotFoundComponent)
+        component: NotFoundComponent
     }
 ];
