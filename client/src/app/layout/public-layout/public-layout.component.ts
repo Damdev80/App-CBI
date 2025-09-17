4e5c119658d0ab238f87b92aef6fb6fbca4408db
Damdev80 <danielbuelvas9624@gmail.com>
2025-09-16 20:40:49 -0500

    feat: refactor navigation links to use buttons and improve login/register forms with enhanced styling

--- client/src/app/layout/public-layout/public-layout.component.ts
+++ client/src/app/layout/public-layout/public-layout.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-public-layout',
@@ -6,5 +7,9 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './public-layout.component.html',
 })
 export class PublicLayoutComponent {
+  constructor(private router:Router) {}
+        navigateTo(path: string) {  
+        this.router.navigate([path])
+    }
   currentYear = new Date().getFullYear();
 }
--- client/src/app/pages/login/login.component.ts
+++ client/src/app/pages/login/login.component.ts
@@ -1,11 +1,16 @@
 import { Component } from "@angular/core";
-
+import { Router } from "@angular/router";
+import { CommonModule } from "@angular/common";
 @Component({
     selector: 'app-login',
     standalone: true,
     templateUrl: './login.component.html',
+    imports: [CommonModule],
 })
 
 export class LoginComponent {
-
+    constructor(private router:Router) {}
+        navegateTo(path: string) {  
+        this.router.navigate([path])
+    }
 }
