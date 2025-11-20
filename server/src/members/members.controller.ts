653ca48fcff9c814ea3c94033865910034d6c59c
Damdev80 <danielbuelvas9624@gmai.com>
2025-11-19 19:10:45 -0500

    feat: implement MembersController and MembersService with CRUD operations

--- server/src/members/members.controller.ts
+++ server/src/members/members.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
-
+import { Controller, Get } from '@nestjs/common';
+import { MembersService } from './members.service'; 
 @Controller('members')
-export class MembersController {}
+export class MembersController {
+    constructor(private readonly membersService: MembersService) {}
+
+    @Get('/')
+    async getAllMembers() {
+        return await this.membersService.findAllMembers();
+    }
+
+    
+}
--- server/src/members/members.service.ts
+++ server/src/members/members.service.ts
@@ -1,4 +1,39 @@
-import { Injectable } from '@nestjs/common';
-
+import { Injectable, BadRequestException, InternalServerErrorException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { IMembers} from './member.interfaces';
+import { th } from 'zod/v4/locales/index.cjs';
 @Injectable()
-export class MembersService {}
+export class MembersService {
+    constructor(private prisma: PrismaService) {}
+
+    async findAllMembers(): Promise<IMembers[]> {
+        try {
+            return await this.prisma.members.findMany();
+        } catch (error) {
+            throw new InternalServerErrorException('Error al obtener los miembros');
+        }
+    }
+
+
+    async findByIdMember(id: string): Promise<IMembers | null> {
+        try {
+            return await this.prisma.members.findUnique({
+                where: { id },
+            })
+        } catch (error) {
+            throw new InternalServerErrorException('Error al obtener el miembro por ID');
+        }
+    }
+
+    async createMember(data: Omit<IMembers, 'id' | 'joinedAt' | 'user' | 'group'>): Promise<IMembers> {
+        try {
+            return await this.prisma.members.create({
+                data: {
+                    ...data,
+                },
+            });
+        } catch (error) {
+            throw new InternalServerErrorException('Error al crear el miembro');
+        }
+    }
+}
