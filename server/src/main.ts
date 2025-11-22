1ca7371cb9b84439bc5e6637ec732ae27894ad15
Damdev80 <danielbuelvas9624@gmai.com>
2025-11-22 17:56:01 -0500

    feat: enable CORS for the application with specified origins and headers

--- server/src/main.ts
+++ server/src/main.ts
@@ -12,8 +12,19 @@ async function bootstrap() {
     console.error('Error connecting to the database', error);
     return;
   }
+  
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ['http://localhost:4200'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+    allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
+
+
+
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
 bootstrap();
