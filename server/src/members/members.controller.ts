83bbc176946873b1e4528cdf95595e691f8ee408
Damdev80 <danielbuelvas9624@gmai.com>
2025-11-25 21:40:09 -0500

    feat: refactor members controller and service to use Prisma types for improved type safety

--- server/src/members/members.controller.ts
+++ server/src/members/members.controller.ts
@@ -1,28 +1,26 @@
 import { Body, Controller, Get, Param, Post, UsePipes } from '@nestjs/common';
-import { MembersService } from './members.service'; 
+import { MembersService } from './members.service';
 import { ZodValidationPipe } from 'src/common/pipes/zod-validation.pipe';
 import { createMemberSchema } from 'src/schemas/create-members.schema';
-import { IMembers } from './member.interfaces';
+import { Members, Prisma } from '@prisma/client';
+
 @Controller('member')
 export class MembersController {
     constructor(private readonly membersService: MembersService) {}
 
     @Get('/members')
-    async getAllMembers(): Promise<IMembers[]>{
+    async getAllMembers(): Promise<Members[]> {
         return await this.membersService.findAllMembers();
     }
 
     @Get('/members/id')
-    async getByIdMembers(@Param('id') id: string): Promise<IMembers | null> {
+    async getByIdMembers(@Param('id') id: string): Promise<Members | null> {
         return await this.membersService.findByIdMember(id);
     }
 
     @Post('/create')
     @UsePipes(new ZodValidationPipe(createMemberSchema))
-    async createMember(@Body() Body: Omit<IMembers, 'id'| 'joinedAt'>): Promise<IMembers> {
-        return await this.membersService.createMember(Body);
+    async createMember(@Body() body: Prisma.MembersUncheckedCreateInput): Promise<Members> {
+        return await this.membersService.createMember(body);
     }
-
-
-  
 }
--- server/src/members/members.service.ts
+++ server/src/members/members.service.ts
@@ -1,11 +1,15 @@
 import { Injectable, BadRequestException, InternalServerErrorException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { IMembers} from './member.interfaces';
+import { Members, Prisma } from '@prisma/client';
+
+export type CreateMemberDto = Prisma.MembersCreateInput;
+export type UpdateMemberDto = Prisma.MembersUpdateInput;
+
 @Injectable()
 export class MembersService {
     constructor(private prisma: PrismaService) {}
 
-    async findAllMembers(): Promise<IMembers[]> {
+    async findAllMembers(): Promise<Members[]> {
         try {
             return await this.prisma.members.findMany();
         } catch (error) {
@@ -14,22 +18,20 @@ export class MembersService {
     }
 
 
-    async findByIdMember(id: string): Promise<IMembers | null> {
+    async findByIdMember(id: string): Promise<Members | null> {
         try {
             return await this.prisma.members.findUnique({
                 where: { id },
-            })
+            });
         } catch (error) {
             throw new InternalServerErrorException('Error al obtener el miembro por ID');
         }
     }
 
-    async createMember(data: Omit<IMembers, 'id' | 'joinedAt' | 'user' | 'group'>): Promise<IMembers> {
+    async createMember(data: Prisma.MembersUncheckedCreateInput): Promise<Members> {
         try {
             return await this.prisma.members.create({
-                data: {
-                    ...data,
-                },
+                data,
             });
         } catch (error) {
             throw new InternalServerErrorException('Error al crear el miembro');
