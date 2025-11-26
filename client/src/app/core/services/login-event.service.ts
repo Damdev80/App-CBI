6d27d14dc56181dcd839da825459bcbd455f1652
Damdev80 <danielbuelvas9624@gmai.com>
2025-11-25 21:51:56 -0500

    feat: add LoginEventService and LoginModel for authentication handling

---
+++ client/src/app/core/services/login-event.service.ts
@@ -0,0 +1,22 @@
+import { Injectable, inject } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { environment } from "src/environments/environment";
+import { LoginModel } from "@app/shared/models/login.model";
+
+@Injectable({
+    providedIn: 'root'
+
+})
+
+export class LoginEventService {
+    private http = inject (HttpClient);
+    private apiUrl = `${environment.apiUrl}/auth`;
+
+    login(credentials: LoginModel) {
+        return this.http.post<{ token: string }>(`${this.apiUrl}/login`, credentials);
+    }
+
+    register(credentials: LoginModel) {
+        return this.http.post<{ token: string }>(`${this.apiUrl}/register`, credentials);
+    }
+}
