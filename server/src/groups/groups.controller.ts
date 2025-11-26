b342541c54ccbea74090a60ec78786291f4f67a6
Damdev80 <danielbuelvas9624@gmai.com>
2025-11-25 21:40:14 -0500

    feat: refactor groups controller and service to use Prisma types for improved type safety

--- server/src/groups/groups.controller.ts
+++ server/src/groups/groups.controller.ts
@@ -1,28 +1,26 @@
 import { Controller, Get, Param, Post, UsePipes, Body } from '@nestjs/common';
-import { GroupsService } from './groups.service';
-import { IGroup } from './group.interfaces';
+import { GroupsService, CreateGroupDto } from './groups.service';
+import { Groups } from '@prisma/client';
 import { ZodValidationPipe } from 'src/common/pipes/zod-validation.pipe';
 import { createGroupSchema } from 'src/schemas/create-group.schema';
+
 @Controller('group')
 export class GroupsController {
     constructor(private readonly groupsService: GroupsService) {}
 
     @Get('/groups')
-    async getAllGroups(): Promise<IGroup[]> {
+    async getAllGroups(): Promise<Groups[]> {
         return await this.groupsService.findAllGroups();
     }
 
     @Get('/groups/id')
-    async getByIdGroup(@Param('id') id: string): Promise<IGroup | null> {
+    async getByIdGroup(@Param('id') id: string): Promise<Groups | null> {
         return await this.groupsService.findByIdGroup(id);
     }
 
     @Post('/create')
     @UsePipes(new ZodValidationPipe(createGroupSchema))
-    async createGroup(@Body() body: Omit<IGroup, 'id' | 'createdAt' | 'updatedAt'>,): Promise<IGroup> {
+    async createGroup(@Body() body: CreateGroupDto): Promise<Groups> {
         return await this.groupsService.createGroup(body);
     }
-
-    
-
 }
--- server/src/groups/groups.service.ts
+++ server/src/groups/groups.service.ts
@@ -1,31 +1,33 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { IGroup } from './group.interfaces';
+import { Groups, Prisma } from '@prisma/client';
+
+export type CreateGroupDto = Prisma.GroupsCreateInput;
+export type UpdateGroupDto = Prisma.GroupsUpdateInput;
+
 @Injectable()
 export class GroupsService {
     constructor(private prisma: PrismaService) {}
 
-    async findAllGroups(): Promise<IGroup[]> {
+    async findAllGroups(): Promise<Groups[]> {
         return await this.prisma.groups.findMany();
     }
 
-    async findByIdGroup(id: string): Promise<IGroup | null> {
+    async findByIdGroup(id: string): Promise<Groups | null> {
         return await this.prisma.groups.findUnique({
-            where: { id},
-        })
+            where: { id },
+        });
     }
 
-    async createGroup(data: Omit<IGroup, 'id' | 'createdAt' | 'updatedAt'>): Promise<IGroup> {
-        if(data.name){
-            const existingGroup =  await this.prisma.groups.findUnique({
-            where: { name: data.name },
-            })
+    async createGroup(data: CreateGroupDto): Promise<Groups> {
+        if (data.name) {
+            const existingGroup = await this.prisma.groups.findUnique({
+                where: { name: data.name },
+            });
         }
 
         return await this.prisma.groups.create({
-            data: {
-                ...data,
-            },
+            data,
         });
     }
 }
